9d4b896d5aa7a046a54d17b7f06647dcfa965fc0
windo-developer <yui050505@gmail.com>
2021-03-08 16:16:38 -0800

    profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,8 +3,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import ScrollViewContainer from "../components/ScrollViewContainer";
-
 const View = styled.View`
   flex: 1;
   justify-content: center;
@@ -15,11 +13,9 @@ const Text = styled.Text``;
 
 const Profile = ({ loading, refreshFunc }) => {
   return (
-    <ScrollViewContainer loading={loading} refreshFunc={refreshFunc}>
-      <View>
-        <Text>Profile</Text>
-      </View>
-    </ScrollViewContainer>
+    <View>
+      <Text>Profile</Text>
+    </View>
   );
 };
 
